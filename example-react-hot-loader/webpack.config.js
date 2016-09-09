607f8b5b3468e09c00dedf1f68d261dc60d0b8f9
limingkang <1035542106@qq.com>
2016-09-09 17:27:08 +0800

    node item

---
+++ example-react-hot-loader/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+
+module.exports = {
+  // entry: './js/entry.js',
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    './js/index.js' // Your appʼs entry point
+  ],
+  output: {
+    path: __dirname + '/js/',
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.jsx?$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ }    //exclude是指排除指定目录
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
